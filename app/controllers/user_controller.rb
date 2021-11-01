254068a87ac22bd2122f40c4d55e302d9f392f06
bcarrol2 <blairacarroll@gmail.com>
2021-11-01 01:08:14 -0400

    user controller index change and view file edits

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,9 +1,7 @@
 class UserController < ApplicationController
   def index
-    byebug
     if current_user
-      @saved_shower_usages = current_user.shower_usages
-      @saved_transportation_footprints = current_user.transportation_footprints
+      @current_user = current_user
     end
   end
 end
